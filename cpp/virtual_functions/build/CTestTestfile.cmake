239d5f808909e8c9213af2f5a734d840cbc7b913
Rafal <rafal9511k@gmail.com>
2024-08-02 18:05:20 +0200

    remove build folders

--- cpp/virtual_functions/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rafal/workspace/examples/examples/virtual_functions
-# Build directory: /home/rafal/workspace/examples/examples/virtual_functions/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/rafal/workspace/examples/examples/virtual_functions/build/ut[1]_include.cmake")
-subdirs("_deps/googletest-build")
